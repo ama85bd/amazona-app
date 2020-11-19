beae36d731c3136b9aaec55df386b247b7f6814c
Asif <ama85bd@gmail.com>
2020-11-19 18:58:03 +0600

    product page and node server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
-import Product from './components/Products';
-import data from './data'
+import {BrowserRouter, Route} from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="grid-container">
         <header className="row">
             <div>
@@ -15,17 +18,12 @@ function App() {
             </div>
         </header>
         <main>
-            <div className="row center">
-              {
-                data.products.map(product =>(
-                  <Product key={product._id} product={product} ></Product>
-                ) )
-              }
-                
-            </div>
+          <Route path="/product/:id" component={ProductScreen} />
+          <Route path="/" component={HomeScreen} exact/>
         </main>
         <footer className="row center">All right reserved</footer>
     </div>
+    </BrowserRouter>
   );
 }
 
