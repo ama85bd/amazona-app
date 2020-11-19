f61eb10a53842463be1121c659b6ef70eb64cd18
Asif <ama85bd@gmail.com>
2020-11-19 15:40:11 +0600

    share git

---
+++ frontend/src/data.js
@@ -0,0 +1,72 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Nike Classic Fit Board Wool Shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'This is very high quality product'
+        },
+        {
+            _id: '2',
+            name: 'Adidas Classic Fit Board Wool Shirt',
+            category: 'Shirts',
+            image: '/images/p2.jpg',
+            price: 100,
+            brand: 'Adidas',
+            rating: 4.3,
+            numReviews: 11,
+            description: 'This is very high quality product Adidas Classic '
+        },
+        {
+            _id: '3',
+            name: 'Lacosta Classic Fit Shirt',
+            category: 'Shirts',
+            image: '/images/p3.jpg',
+            price: 140,
+            brand: 'Lacosta',
+            rating: 4.8,
+            numReviews: 17,
+            description: 'This is very high quality product Lacosta'
+        },
+        {
+            _id: '4',
+            name: 'Nike Fit Pant',
+            category: 'Pants',
+            image: '/images/p4.jpg',
+            price: 87,
+            brand: 'Nike',
+            rating: 4.7,
+            numReviews: 15,
+            description: 'This is very high quality product Nike Fit Pant'
+        },
+        {
+            _id: '5',
+            name: 'Adidas Fit Board Wool Pant',
+            category: 'Pants',
+            image: '/images/p5.jpg',
+            price: 90,
+            brand: 'Adidas',
+            rating: 4.9,
+            numReviews: 10,
+            description: 'This is very high quality product Adidas'
+        },
+        {
+            _id: '6',
+            name: 'Puma Classic Fit Pant',
+            category: 'Pants',
+            image: '/images/p6.jpg',
+            price: 99,
+            brand: 'Puma',
+            rating: 4.8,
+            numReviews: 18,
+            description: 'This is very high quality product Puma'
+        },
+    ],
+};
+
+export default data;
