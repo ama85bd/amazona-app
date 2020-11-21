b0f49481c8eaa9557345992d042a0e1d2317a243
Asif <ama85bd@gmail.com>
2020-11-21 10:38:20 +0600

    eslint redux react-rdux redux-thunk

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,28 +1,19 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios';
+import React, { useEffect } from 'react'
 import Product from '../components/Products';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
+import { useDispatch, useSelector } from 'react-redux';
+import { listProducts } from '../actions/productActions';
 
 export default function HomeScreen(){
-    const [products, setProducts] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
+    const dispatch = useDispatch();
+    const productList = useSelector((state) => state.productList);
+    const { loading, error, products } = productList;
+    console.log(products)
+
     useEffect(() => {
-      const fetchData = async () => {
-        try{
-          setLoading(true);
-        const {data} = await axios.get('/api/products');
-        setLoading(false);
-        setProducts(data);
-        }catch (err){
-          setError(err.message);
-          setLoading(false);
-        }
-        
-      };
-      fetchData();
-    }, [])
+      dispatch(listProducts());
+    }, [dispatch])
     return (
         <div>
           {loading? <LoadingBox></LoadingBox>
@@ -30,9 +21,9 @@ export default function HomeScreen(){
           error? <MessageBox variant="danger">{error}</MessageBox>
           :<div className="row center">
           {
-            products.map(product =>(
-              <Product key={product._id} product={product} ></Product>
-            ) )
+          products.map(product =>(
+            <Product key={product._id} product={product} ></Product>
+          ) )
           }
             
       </div>
