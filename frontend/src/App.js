9c91e6c27f40bdde4813e7b181c0c1e85bc68893
Asif <ama85bd@gmail.com>
2020-11-22 00:29:01 +0600

    eslint redux react-rdux redux-thunk prettier es7/

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,39 @@
-import React from 'react'
-import {BrowserRouter, Route} from 'react-router-dom';
-import HomeScreen from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
-
+import React from "react";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Link, Route } from "react-router-dom";
+import CartScreen from "./screens/CartScreen";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
   return (
     <BrowserRouter>
-    <div className="grid-container">
+      <div className="grid-container">
         <header className="row">
-            <div>
-                <a className="brand" href="/">amazona</a>
-            </div>
-            <div>
-                <a href="/cart">Cart</a>
-                <a href="/signin">Sign In</a>
-            </div>
+          <div>
+            <Link className="brand" to="/">
+              amazona
+            </Link>
+          </div>
+          <div>
+            <Link to="/cart">
+              Cart
+              {cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
+              )}
+            </Link>
+            <Link to="/signin">Sign In</Link>
+          </div>
         </header>
         <main>
+          <Route path="/cart/:id?" component={CartScreen}></Route>
           <Route path="/product/:id" component={ProductScreen} />
-          <Route path="/" component={HomeScreen} exact/>
+          <Route path="/" component={HomeScreen} exact />
         </main>
         <footer className="row center">All right reserved</footer>
-    </div>
+      </div>
     </BrowserRouter>
   );
 }
